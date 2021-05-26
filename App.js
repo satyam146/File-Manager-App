aab5c642df3bfb4216db6f92710bfa281ed9beb8
satyam146 <82771699+satyam146@users.noreply.github.com>
2021-05-26 23:17:31 +0530

    File manager app

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeStackNavigator from "./src/navigations/Navigator"
+import { useFonts, Montserrat_700Bold,Montserrat_600SemiBold,Montserrat_800ExtraBold } from '@expo-google-fonts/montserrat';
+import {AppLoading} from 'expo';
+import {Views} from 'react-native'
+
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,Montserrat_600SemiBold,Montserrat_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return(
+    <NavigationContainer>
+      <HomeStackNavigator/>
+    </NavigationContainer>
+  )
+}
+export default App;
---
+++ README.md
@@ -0,0 +1,9 @@
+A simple file manager app that represents a JSON tree structure as a file system of files and folders.
+1. Multiple selections of assets
+2. User-based data
+
+3.  cascading tree view of folders
+4. rename, download assets
+5. search assets
+6. asset info popup
+7. varying file icons for assets
